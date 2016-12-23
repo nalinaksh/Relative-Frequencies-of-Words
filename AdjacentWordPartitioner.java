41ae21ef727515b8f3de04772b68ed696a21896b
Nalinaksh Gaur <nalinaksh@gmail.com>
2016-12-23 18:32:41 -0500

    Add files via upload

---
+++ AdjacentWordPartitioner.java
@@ -0,0 +1,13 @@
+//Filename : AdjacentWordPartitioner.java
+//Author   : Nalinaksh Gaur
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AdjacentWordPartitioner extends Partitioner<AdjacentWord,IntWritable> {
+
+    @Override
+    public int getPartition(AdjacentWord wordPair, IntWritable intWritable, int numPartitions) {
+        return (wordPair.getWord().hashCode() & Integer.MAX_VALUE ) % numPartitions;
+    }
+}
